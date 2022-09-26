ec32bf04f25249cfa52ebff9c87736a6203642a1
georgi-rm <georgi.rm@gmail.com>
2022-09-26 18:54:38 +0300

    Problem 4 "Back In 30 Minutes" from Lab: Basic Syntax Conditional Statements And Loops

---
+++ T01BasicSyntaxConditionalStatementsAndLoops/Lab/P04BackIn30Minutes/src/P04BackIn30Minutes.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class P04BackIn30Minutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int hours = Integer.parseInt(scanner.nextLine());
+        int minutes = Integer.parseInt(scanner.nextLine());
+
+        minutes += 30;
+
+        if (minutes > 59) {
+            minutes -= 60;
+            hours += 1;
+            if (hours > 23) {
+                hours -= 24;
+            }
+        }
+
+        if (minutes < 10) {
+            System.out.printf("%d:0%d", hours, minutes);
+        } else {
+            System.out.printf("%d:%d", hours, minutes);
+        }
+    }
+}
