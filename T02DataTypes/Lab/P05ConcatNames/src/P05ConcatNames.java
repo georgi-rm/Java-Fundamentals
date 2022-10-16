67ccb2e0484cfecec664484a54529ecb52a4d458
georgi-rm <georgi.rm@gmail.com>
2022-10-16 20:20:12 +0300

    Problem 5 "Concat Names" from Lab: Data Types

---
+++ T02DataTypes/Lab/P05ConcatNames/src/P05ConcatNames.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P05ConcatNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+        String secondName = scanner.nextLine();
+        String delimiter = scanner.nextLine();
+
+        System.out.println(firstName + delimiter + secondName);
+    }
+}
