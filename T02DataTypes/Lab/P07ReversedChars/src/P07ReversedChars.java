ef51bda01c49b5e728d28ac74994ff1a4e6a9efd
georgi-rm <georgi.rm@gmail.com>
2022-10-18 23:11:15 +0300

    Problem 7 "Reversed Chars" from Lab: Data Types

---
+++ T02DataTypes/Lab/P07ReversedChars/src/P07ReversedChars.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P07ReversedChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstLine = scanner.nextLine();
+        String secondLine = scanner.nextLine();
+        String thirdLine = scanner.nextLine();
+
+        System.out.printf("%s %s %s", thirdLine, secondLine, firstLine);
+    }
+}
